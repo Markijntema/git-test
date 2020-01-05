81ed960ab39254503dc30e4295c5cd640e763c3e
Markijntema <markijntema@outlook.com>
2020-01-05 11:23:52 +0100

    Updates

---
+++ precache-manifest.46efb1e40826894ff59a897f0c0ce172.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579309d12c272f3bf38205500ff358c9",
+    "url": "/git-test/index.html"
+  },
+  {
+    "revision": "498501ed1ac2daaeeb1d",
+    "url": "/git-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bdd46021e8443681dea9",
+    "url": "/git-test/static/js/2.2c670279.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/git-test/static/js/2.2c670279.chunk.js.LICENSE"
+  },
+  {
+    "revision": "498501ed1ac2daaeeb1d",
+    "url": "/git-test/static/js/main.4f72cbe5.chunk.js"
+  },
+  {
+    "revision": "c7514fd927bee6c516b6",
+    "url": "/git-test/static/js/runtime-main.a6c12af1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
